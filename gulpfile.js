53fc9e49e61b36c86346b337a1469ef977ce06ca
yunp <xtiqin@163.com>
2018-05-23 09:21:38 +0800

    add build script

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require("gulp");
+const path = require("path");
+const install = require("gulp-install");
+
+
+const OUTPUT_DIR = path.join(__dirname, 'out', 'app');
+
+gulp.task("build", function () {
+    gulp.src([
+        path.join(__dirname, "main.js"),
+        path.join(__dirname, "package.json")
+    ]).pipe(gulp.dest(OUTPUT_DIR));
+    gulp.src(path.join(__dirname, "res", "**/*")).pipe(gulp.dest(path.join(OUTPUT_DIR, "res")));
+    gulp.src(path.join(__dirname, "renderers", "**/*")).pipe(gulp.dest(path.join(OUTPUT_DIR, "renderers")));
+});
+
+gulp.task("ProductionInstall", ["build"], function () {
+    gulp.src(path.join(OUTPUT_DIR, 'package.json')).pipe(install({
+        npm: '--production'
+    }));
+});
+
+
+gulp.task("default", ["ProductionInstall"]);
